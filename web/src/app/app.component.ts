69846b7b003be7e5c90aa6ec3bd79a4b677da669
Michał Kotfica <kotvitz17@gmail.com>
2023-11-21 00:24:29 +0100

    create side menu

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web';
+  @ViewChild('sideMenu') sideMenu!: ElementRef;
+  @ViewChild('hamburgerMenu') hamburgerMenu!: ElementRef;
+
+  toggleSideMenu() {
+    if (this.sideMenu.nativeElement && this.hamburgerMenu.nativeElement) {
+      const sideMenuElement = this.sideMenu.nativeElement as HTMLElement;
+      const hamburgerMenuElement = this.hamburgerMenu.nativeElement as HTMLElement;
+
+      sideMenuElement.style.left = sideMenuElement.style.left === '0px' ? '-400px' : '0px';
+
+      hamburgerMenuElement.innerHTML =
+        hamburgerMenuElement.innerHTML === '☰' ? '&#10006;' : '☰';
+    }
+  }
 }
