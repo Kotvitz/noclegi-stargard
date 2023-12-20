a00ffc8f818cc4fc32ceb83d2e921733d268b9e8
Michał Kotfica <kotvitz17@gmail.com>
2023-12-20 16:39:02 +0100

    opting out of SSR

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -14,9 +14,7 @@ import { SmoothScrollDirective } from './smooth-scroll.directive';
     BrowserModule,
     RouterModule.forRoot([]),
     ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
